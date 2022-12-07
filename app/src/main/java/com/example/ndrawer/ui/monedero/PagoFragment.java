8d2f63bb095331ed2b8a75713efc90ac420cfafb
McPabloo <arsenwatt@gmail.com>
2022-12-06 23:08:33 -0600

    Fix error del monedero Ya no se empalman y funcionan los botónes

--- app/src/main/java/com/example/ndrawer/ui/monedero/PagoFragment.java
+++ app/src/main/java/com/example/ndrawer/ui/monedero/PagoFragment.java
@@ -1,7 +1,10 @@
 package com.example.ndrawer.ui.monedero;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +14,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.ndrawer.R;
 import com.example.ndrawer.databinding.FragmentComprarBinding;
+import com.example.ndrawer.databinding.FragmentMonederoBinding;
 import com.example.ndrawer.databinding.FragmentPagoBinding;
 
-public class PagoFragment extends Fragment {
+public class PagoFragment extends Fragment implements View.OnClickListener {
 
     private FragmentPagoBinding binding;
 
@@ -31,6 +36,8 @@ public class PagoFragment extends Fragment {
         binding = FragmentPagoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        Button boton1 = root.findViewById(R.id.btnfinalizar);
+        boton1.setOnClickListener(this);
 
         return root;
 
@@ -40,6 +47,28 @@ public class PagoFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        switch (v.getId()) {
+            case R.id.btnfinalizar:
+                fragment = new GalleryFragment();
+                replaceFragment(fragment);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setReorderingAllowed(true);
+        transaction.replace(R.id.layout_monedero, someFragment, null);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
